a2f8d76833708478ee1c80fd8f0ac2135844e4d9
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-12 21:30:40 -0300

    ejercicio final

---
+++ assets/js/ejercicioFinalModulo.js
@@ -0,0 +1,27 @@
+//--------------------------
+// Ejercicio cierre
+
+
+// Ejercicio de Práctica: Biblioteca Virtual
+// Enunciado:
+// Imagina que estás creando una aplicación para gestionar una biblioteca virtual. Deberás implementar un sistema que permita a los usuarios agregar libros a su colección, ver la lista de libros añadidos, calcular el número total de páginas y buscar un libro específico en la colección.
+
+// Requerimientos:
+// Crea una clase Libro con los atributos titulo, autor, y paginas.
+// Crea la clase Biblioteca que tenga como atributo un arreglo para almacenar los libros añadidos por el usuario.
+// Crea un método agregarLibro(libro) en la clase Biblioteca que permita añadir libros a la colección.
+// Crea un método calcularTotalPaginas() que retorne el número total de páginas de todos los libros en la colección.
+// Crea un método buscarLibro(titulo) que permita buscar un libro por su título y retorne un mensaje indicando si el libro se encuentra en la colección o no.
+// Crea un método mostrarColeccion() que imprima en la consola el título y el autor de cada libro en la colección, así como el total de páginas de todos los libros.
+// Permite que el usuario siga añadiendo libros a su colección hasta que decida no continuar.
+// Puntos Importantes:
+// Asegúrate de que los datos ingresados sean válidos, y pide al usuario que los ingrese nuevamente si hay errores.
+// Utiliza buenas prácticas de programación para hacer que el código sea claro y fácil de mantener.
+
+
+
+
+// Implementa el código utilizando las clases Libro y Biblioteca siguiendo los requerimientos.
+// Prueba tu programa para asegurarte de que permite agregar varios libros, calcula correctamente el total de páginas, muestra la colección de libros y permite buscar un libro específico.
+// Valida los datos para evitar errores al ingresar el título, autor o número de páginas de los libros.
+// Revisa tu código para asegurarte de que esté bien estructurado y sea fácil de leer.
